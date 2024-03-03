2949f6efd5b7f291058e47ab1351d3b9d9ed3fa2
Benyamin Gheiji <benyamingheiji@gmail.com>
2024-03-03 19:59:54 +0330

    no message

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ model = joblib.load('hematoma.pkl')
 st.title('Prediction of EDH expansion based on patient data :brain:')
 
 age = st.number_input("Enter the patient age")
-airbleb = st.selectbox('Does the patient have Intra-hematoma airbleb? 0: No     1: yes', [0,1])
 swirl_sign = st.selectbox('Does the patient have Swirl Sign? 0: No     1: yes', [0,1])
-edh_volume = st.number_input("Enter initial EDH volume")
+airbleb = st.selectbox('Does the patient have Intra-hematoma airbleb? 0: No     1: yes', [0,1])
 contusion = st.selectbox('Does the patient have contusion? 0: No     1: yes', [0,1])
 other_side_extra_axial_hematoma = st.selectbox('Does the patient have other side extra-axial hematoma? 0: No     1: yes', [0,1])
-location= st.selectbox('Where is the location of the hematoma? Parietal,Occipital,Posterior fossa,Frontotemporal,Frontotemporoparietal,Others',
+edh_volume = st.number_input("Enter initial EDH volume")
+location= st.selectbox('Where is the location of the hematoma? Parietal1,Occipital2,Posterior fossa3,Frontotemporal4,Frontotemporoparietal5,Others6',
                        [1,2,3,4,5,6])
 
 
@@ -28,9 +28,9 @@ def predict():
     X = pd.DataFrame([row], columns = columns)
     prediction = model.predict(X)
     if prediction[0] >= 0.2: 
-        print('The EDH volume is more likely to be increased :heavy_exclamation_mark:')
+        st.error('The EDH volume is more likely to be increased :heavy_exclamation_mark:')
     elif prediction[0] < 0.2:
-        print('The EDH volume is more likely not to be increased :white_check_mark:')
+        st.success('The EDH volume is more likely not to be increased :white_check_mark:')
         
 
 trigger = st.button('Predict', on_click=predict)
