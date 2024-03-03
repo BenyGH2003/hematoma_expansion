8c07e8e0a533bdb9ab877e24dcca314459e864f6
Benyamin Gheiji <benyamingheiji@gmail.com>
2024-03-03 17:40:31 +0330

    no message

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.preprocessing import StandardScaler
+
+columns= [['age/num', 'swirl/cat', 'airbleb/cat', 'contusion/cat', 'edh/cat', 'v1/num', 'location/cat_3', 'location/cat_4', 'location/cat_5', 'location/cat_9', 'location/cat_10']]
+
+
+
+model = joblib.load('hematoma.pkl')
+
+
+st.title('Prediction of EDH expansion based on patient data :brain:')
+
+age = st.number_input("Enter the patient age")
+airbleb = st.selectbox('Does the patient have Intra-hematoma airbleb? 0: No     1: yes', [0,1])
+swirl_sign = st.selectbox('Does the patient have Swirl Sign? 0: No     1: yes', [0,1])
+edh_volume = st.number_input("Enter initial EDH volume")
+contusion = st.selectbox('Does the patient have contusion? 0: No     1: yes', [0,1])
+other_side_extra_axial_hematoma = st.selectbox('Does the patient have other side extra-axial hematoma? 0: No     1: yes', [0,1])
+location= st.selectbox('Where is the location of the hematoma? 1:Temporal   2:Temporoparietal   3: Parietal   4: Occipital   5: Posterior fossa   6:Temporooccipital   7:Frontal   8:Frontoparietal   9: Frontotemporal   10: Frontotemporoparietal   11:others',
+                       [1,2,3,4,5,6,7,8,9,10,11])
+
+
+def predict(): 
+    row = np.array([age,swirl_sign, airbleb,contusion,other_side_extra_axial_hematoma,edh_volume,location]) 
+    X = pd.DataFrame([row], columns = columns)
+    prediction = model.predict(X)
+
+    if prediction[0] >= 0.25: 
+        st.error('The EDH volume is more likely to be increased :heavy_exclamation_mark:')
+    elif prediction[0] < 0.25:
+        st.success('The EDH volume is more likely not to be increased :white_check_mark:') 
+        
+
+trigger = st.button('Predict', on_click=predict)
