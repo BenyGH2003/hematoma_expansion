9fd85e4e9200a7acf4942743cf697d847994175d
Benyamin Gheiji <benyamingheiji@gmail.com>
2024-03-03 19:48:05 +0330

    no message

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ swirl_sign = st.selectbox('Does the patient have Swirl Sign? 0: No     1: yes', 
 edh_volume = st.number_input("Enter initial EDH volume")
 contusion = st.selectbox('Does the patient have contusion? 0: No     1: yes', [0,1])
 other_side_extra_axial_hematoma = st.selectbox('Does the patient have other side extra-axial hematoma? 0: No     1: yes', [0,1])
-location= st.selectbox('Where is the location of the hematoma? 1:Parietal, 2:Occipital, 3:Posterior fossa, 4:Frontotemporal, 5:Frontotemporoparietal, 6:Others',
+location= st.selectbox('Where is the location of the hematoma? Parietal,Occipital,Posterior fossa,Frontotemporal,Frontotemporoparietal,Others',
                        [1,2,3,4,5,6])
 
 
@@ -27,9 +27,9 @@ def predict():
     row = np.array([age,swirl_sign, airbleb,contusion,other_side_extra_axial_hematoma,edh_volume,location])
     X = pd.DataFrame([row], columns = columns)
     prediction = model.predict(X)
-    if prediction[0] >= 0.20: 
+    if prediction[0] >= 0.2: 
         print('The EDH volume is more likely to be increased :heavy_exclamation_mark:')
-    elif prediction[0] < 0.20:
+    elif prediction[0] < 0.2:
         print('The EDH volume is more likely not to be increased :white_check_mark:')
         
 
